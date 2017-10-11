b765f41cf04f4a26b2a0727deafc9daee4bf4018
Alex Ellsworth <alex.ells21@gmail.com>
2017-10-11 16:38:26 -0600

    second commit

--- index.js
+++ index.js
@@ -2,9 +2,19 @@ const express = require('express')
         , bodyParser = require('body-parser')
         , cors = require('cors')
         , massive = require('massive')
+require('dotenv').config()
 
 
 let app = express()
 app.use(bodyParser.json());
 app.use(cors());
+massive(process.env.CONNECTION_STRING).then( db => app.set('db', db) );
+
+
+
+
+const PORT = 3013
+app.listen(PORT, () => {
+    console.log(`listening on ${PORT}`)
+})
 

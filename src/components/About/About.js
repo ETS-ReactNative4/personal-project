88816c145ccd7606c3ca525f860229ab431ccffa
Alex Ellsworth <alex.ells21@gmail.com>
2017-10-13 10:52:08 -0600

    added projects component

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import NavBar from '../NavBar/NavBar';
+import './About.css'
 
 
 export default class About extends Component {
@@ -7,7 +8,18 @@ export default class About extends Component {
     render() {
         return (
             <div>
+                <div className='about-hero'>
                 <NavBar />
+                    <div className='about-us'>
+                        <h1>TLC ROCK</h1>
+                        <h3>
+                            is a custom crushing operation capable of crushing up to 750 tons per hour. We have been in business for 15 years. TLC is managed by Kevin Larsen who oversees the operation and maintains quality control.
+                            We are a completely mobile operation able to mobilize to our customer’s site and process there materials on site.  We specialize in construction aggregates such as road base, drain rock, crushed rock and fill materials, as well as washed rock products. 
+                            We have the abilities to RECYCLE concrete and asphalt turning them in to valuable materials that save resources and space in our landfills.
+                            Our Equipment is well maintained and up to date to keep down time to a minimum so we can be as efficient as possible while working on you project.
+                        </h3>
+                    </div>
+                </div>
             </div>
         )
     }
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import NavBar from '../NavBar/NavBar';
 import './Projects.css';
+import project from './project.jpg'
 
 
 export default class Projects extends Component {
@@ -9,11 +10,20 @@ export default class Projects extends Component {
         return (
             <div>
                 <div className='heroProjects'>
-                <NavBar />
-                <div className='headerProjects'>
-                    <h1>Current projects we're working on</h1>
+                    <NavBar />
+                    <div className='headerProjects'>
+                        <h1>Current projects we're working on</h1>
+                    </div>
                 </div>
-
+                    <h1 className='project-header'>Project 1</h1>
+                <div className='project'>
+                    <div><img className='project1' src={project} alt="loading" /></div>
+                    <h3 className='project-info'>This is one of our projects</h3>
+                </div>
+                <h1 className='project-header'>Project 2</h1>
+                <div className='project'>
+                    <div><img className='project1' src={project} alt="loading" /></div>
+                    <h3 className='project-info'>This is another one of our projects</h3>
                 </div>
             </div>
         )
